ec0aba2df5bee7554d447795eee5fb4307e1be42
koushikvuddagiri <131535809+koushikvuddagiri@users.noreply.github.com>
2023-04-22 22:01:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.header('🍌🥭 Build your own Fruit Smooithe 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+my_fruit_list = my_fruit_list.set_index('Serving_Gram_Weight')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index))
