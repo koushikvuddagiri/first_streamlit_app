6521c556107cf964fc5a55fef7b9681661ac2a50
koushikvuddagiri <131535809+koushikvuddagiri@users.noreply.github.com>
2023-04-22 21:20:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.header('🍌🥭 Build your own Fruit Smooithe 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read.csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+streamlit.dataframe(my_fruit_list)
 
 
