1737ef59be75e94acafc255b08773c88938d91a8
koushikvuddagiri <131535809+koushikvuddagiri@users.noreply.github.com>
2023-04-22 19:30:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title ('My parents New Healthy Diner')
+
+breakfast_items = ['Omega 3 & Blueberry Oatmeal', 'Kale, Spinach & Rocket Smoothie', 'Hard-Boiled Free-Range Egg']
+
+streamlit.title.write('Breakfast Menu:')
+for item in breakfast_items:
+streamlit.title.write('- ' + item)
