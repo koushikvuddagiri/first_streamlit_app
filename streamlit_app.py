0f474f9ccfc70c8e68497104ba723ec802418337
koushikvuddagiri <131535809+koushikvuddagiri@users.noreply.github.com>
2023-04-22 19:35:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
-import streamlit
-streamlit.title ('My parents New Healthy Diner')
+import streamlit as st
 
+# Set the title of the web page
+st.title('My Parents New Healthy Diner')
+
+# Define the breakfast menu items
 breakfast_items = ['Omega 3 & Blueberry Oatmeal', 'Kale, Spinach & Rocket Smoothie', 'Hard-Boiled Free-Range Egg']
 
-streamlit.write('Breakfast Menu:')
+# Display the breakfast menu items using a bullet point list
+st.write('Breakfast Menu:')
 for item in breakfast_items:
-streamlit.write('-' + item)
-
+    st.write('- ' + item)
