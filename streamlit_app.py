dac634fa3d06a0db4ea4ea77cc5db1b38d28e23b
koushikvuddagiri <131535809+koushikvuddagiri@users.noreply.github.com>
2023-04-24 16:45:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,13 +26,14 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruitvice api respone
 streamlit.header('Fruitvice Fruit Advice!')
 
-#Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Ca
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
-requirements.txt
-import snowflake.connector
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
