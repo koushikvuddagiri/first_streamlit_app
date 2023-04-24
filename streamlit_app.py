14f36fe5e1aa6df1a14d961ed40523cc5242b648
koushikvuddagiri <131535809+koushikvuddagiri@users.noreply.github.com>
2023-04-24 17:57:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,47 @@
 import streamlit 
+import subprocess
+import pandas as pd
+import requests
+import snowflake.connector
+
+# Set Streamlit app title
 streamlit.title('My Parents New Healthy Diner')
 
-
+# Display breakfast favorites
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-    
-   
-streamlit.header('🍌🥭 Build your own Fruit Smooithe 🥝🍇')
+# Display build your own fruit smoothie section
+streamlit.header('🍌🥭 Build your own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Load fruit list from URL
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+# Let users pick fruits to include in the smoothie
+fruits_selected = streamlit.multiselect("Pick Some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page
+# Display the selected fruits in a table
 streamlit.dataframe(fruits_to_show)
 
-
-
+# Display Fruityvice Fruit Advice section
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered:', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Fetch fruit information from Fruityvice API
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # Install dependencies from requirements.txt file
 subprocess.call("pip install -r requirements.txt", shell=True)
 
-import subprocess
-
-# Rest of your code here
-
+# Connect to Snowflake and fetch user information
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
